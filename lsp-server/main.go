1f55577d3a214d7bb3457cba76de6e1933375636
monsteronfire <monsteronfire@gmail.com>
2025-09-01 00:47:57 +0800

    Add `callOllama` function and supporting types

--- lsp-server/main.go
+++ lsp-server/main.go
@@ -1,6 +1,24 @@
 package main
 
-import "log"
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+const ollamaBaseURL = "http://host.docker.internal:11434/api/"
+
+type OllamaRequest struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+	Stream bool   `json:"stream"`
+}
+
+type OllamaResponse struct {
+	Response string `json:"response"`
+	Done     string `json:"done"`
+}
 
 // Custom type representing the LSP server
 type LSPServer struct {
@@ -12,6 +30,38 @@ func NewLSPServer() *LSPServer {
 	return &LSPServer{
 		documents: make(map[string]string),
 	}
+}
+
+func (s *LSPServer) callOllama(prompt string) (string, error) {
+	requestBody := OllamaRequest{
+		Model:  "Llama-3.2-1B-Instruct-GGUF",
+		Prompt: prompt,
+		Stream: false,
+	}
+
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		return "", err
+	}
+
+	response, err := http.Post(
+		ollamaBaseURL+"generate",
+		"application/json",
+		bytes.NewBuffer(jsonData),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	var ollamaResp OllamaResponse
+	err = json.NewDecoder(response.Body).Decode(&ollamaResp)
+	if err != nil {
+		return "", err
+	}
+
+	return ollamaResp.Response, nil
 }
 
 func (s *LSPServer) serve() {
